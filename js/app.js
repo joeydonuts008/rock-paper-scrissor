95716ccd9efe19ccfffce16158bc1dd2927f5d4c
Joe <126Jhaney@frhsd.com>
2024-12-09 12:53:21 -0500

    gegre

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userchoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = doucment.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userchoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possiblechoice => possiblechoice.addEventListener('click' , (e) =>))
+userChoice = e.target.id 
+userChoiceDisplay.innerHTML = userChoice
+generate computerChoice()
+getresult()
+}))
+
+
+function GenerateComputerChoice() {
+    const randomNumber = Math.floor(math.random() * 3) + 1
+
+    if (randomNumber === 1)
+}
